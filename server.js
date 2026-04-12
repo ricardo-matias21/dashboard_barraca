e13e28154eeb2af2ed636696ffbeb383e677cd0f
ricardo-matias21 <ricardomatiaas@gmail.com>
2026-04-12 22:09:48 +0100

    Versão 2.0

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.put('/api/goals/:id', upload.single('image'), (req, res) => {
 // Estado Global
 let config = {
     url: 'https://services.3cket.com/staff/cashless-worzkone.php?start_date=09-04-2026+17%3A15&end_date=01-05-2026+22%3A37&search_staff=&search_workzone=9fd8ea4cd7dc4787985f419d2b2ddc12&search_product=&subPage=byProducts&view_mode=total&month_view=0',
-    modoArraial: false,
+    modoArraial: true,
     frankFileGoals: [] // { id, target, image, triggered }
 };
 
